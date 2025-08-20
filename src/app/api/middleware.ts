8945a094e7348ec5f96b91ef20703531fd82dc1f
Himanshu Negi <himanshunegi267@gmail.com>
2025-08-20 19:18:47 +0530

    Middleware Test

--- src/app/api/middleware.ts
+++ src/app/api/middleware.ts
@@ -6,26 +6,26 @@ const uploadTracker: Record<string, number> = {};
 export function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
-  let sessionId = req.cookies.get("session-id")?.value;
+  // let sessionId = req.cookies.get("session-id")?.value;
 
-  if (!sessionId) {
-    sessionId = crypto.randomUUID();
+  // if (!sessionId) {
+  //   sessionId = crypto.randomUUID();
 
-    res.cookies.set("session-id", sessionId, {
-      httpOnly: true,
-      sameSite: "strict",
-      path: "/",
-      secure:true,
-    });
-  }
-  // Count uploads for this session
-  uploadTracker[sessionId] = (uploadTracker[sessionId] || 0) + 1;
+  //   res.cookies.set("session-id", sessionId, {
+  //     httpOnly: true,
+  //     sameSite: "strict",
+  //     path: "/",
+  //     secure:true,
+  //   });
+  // }
+  // // Count uploads for this session
+  // uploadTracker[sessionId] = (uploadTracker[sessionId] || 0) + 1;
 
-  if (uploadTracker[sessionId] > 3) {
-    return NextResponse.json({ success: false, error: "Upload limit reached" }, { status: 429 });
-  }
+  // if (uploadTracker[sessionId] > 3) {
+  //   return NextResponse.json({ success: false, error: "Upload limit reached" }, { status: 429 });
+  // }
 
-  res.headers.set("x-upload-count", uploadTracker[sessionId].toString());
+  // res.headers.set("x-upload-count", uploadTracker[sessionId].toString());
   
   return res;
 }
